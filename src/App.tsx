4147f009ca4c76752f3b7bedf1a3b1865ea8d2b2
Cristi <cpninesix1@gmail.com>
2025-11-21 19:41:46 +0200

    fix build 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { Story } from './components/Story';
-import { Gallery } from './components/Gallery';
-import { Screenings } from './components/Screenings';
-import { Shop } from './components/Shop';
-import { Footer } from './components/Footer';
-
-function App() {
-  return (
-    <div className="min-h-screen bg-stone-950 text-stone-200 font-sans antialiased selection:bg-orange-600 selection:text-white">
-      <Navbar />
-      <main>
-        <Hero />
-        <Story />
-        <Gallery />
-        <Screenings />
-        <Shop />
-      </main>
-      <Footer />
-    </div>
-  );
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "useDefineForClassFields": true,
+    "lib": ["ES2020", "DOM", "DOM.Iterable"],
+    "module": "ESNext",
+    "skipLibCheck": true,
+    "moduleResolution": "bundler",
+    "allowImportingTsExtensions": true,
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+    "jsx": "react-jsx",
+    "strict": false,
+    "noUnusedLocals": false,
+    "noUnusedParameters": false,
+    "noFallthroughCasesInSwitch": true
+  },
+  "include": ["."]
 }
-
-export default App;
